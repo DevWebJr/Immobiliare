61e95d20f9042b742aa7941ce2d2590e3505cd7a
Dev Web Junior <50543732+DevWebJr@users.noreply.github.com>
2021-05-26 18:32:29 +0200

    create property Entity

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Property;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,6 +14,23 @@ class PropertyController extends AbstractController
      */
     public function index(): Response
     {
+        $property  = new Property();
+        $property->setTitle('Mon premier bien');
+        $property->setPrice(200000);
+        $property->setRooms(4);
+        $property->setBedrooms(3);
+        $property->setDescription('Voici la description de mon premier bien');
+        $property->setSurface(60);
+        $property->setFloor(4);
+        $property->setHeat(0);
+        $property->setCity('Strasbourg');
+        $property->setAddress('9 boulevard Wilson');
+        $property->setPostalCode(67000);        
+        
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($property);
+        $entityManager->flush();
+
         return $this->render('property/index.html.twig', [
             'current_main' => 'show_all_properties'
         ]);
